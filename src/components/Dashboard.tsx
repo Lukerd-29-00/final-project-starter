e0d8c6814b619a7ec46dd91c13a2e86f8f405662
lucasd <Driscoll_Lucas@bah.com>
2021-10-07 10:37:12 -0400

    one more try

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,23 @@
+import { ButtonHTMLAttributes, useState } from "react"
+import useButtons from "../hooks/useButtons"
+import {Button, Counter} from "../hooks/useButtons"
+
 interface noProps {
 
 }
 export function Dashboard(props: noProps){
-    
+    const [buttons, addButtons] = useButtons()
+    return (
+        <>
+            {buttons.map((value: Button, index: number) => {
+                <div>
+                    <button onClick = {() => {value.counter.plus()}}>+</button>
+                    <span>{value.counter.value}</span>
+                </div>
+
+            })}
+            <button>Add</button>
+        </>
+    )
+
 }
--- src/hooks/useButtons.ts
+++ src/hooks/useButtons.ts
@@ -1,5 +1,5 @@
 import {useState} from "react"
-interface Counter {
+export interface Counter {
     value: number,
     plus: () => void,
     minus: () => void
@@ -12,7 +12,6 @@ export interface Button {
 
 function useButtons(): [Array<Button>, (label: string, incrementUp: () => void, incrementDown: () => void) => void]{
     const [buttons, setButtons] = useState(new Array<Button>())
-
     const addButton = (label: string, incrementUp: () => void, incrementDown: () => void) => {
         let newButtons = new Array<Button>().concat(buttons)
         newButtons.push({label: label, counter: {value: 0, plus: incrementUp, minus: incrementDown}})
