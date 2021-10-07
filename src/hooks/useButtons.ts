e0afe6c23e6c29c54ff3a791909fbf20bdb930b8
lucasd <Driscoll_Lucas@bah.com>
2021-10-07 10:40:09 -0400

    updated hoook

--- src/hooks/useButtons.ts
+++ src/hooks/useButtons.ts
@@ -12,9 +12,15 @@ export interface Button {
 
 function useButtons(): [Array<Button>, (label: string, incrementUp: () => void, incrementDown: () => void) => void]{
     const [buttons, setButtons] = useState(new Array<Button>())
-    const addButton = (label: string, incrementUp: () => void, incrementDown: () => void) => {
+    const increment = (i: number, x: number) => {
+        let newButtons = new Array<Button>()
+        newButtons[i].counter.value += x
+        setButtons(newButtons)
+    }
+
+    const addButton = (label: string) => {
         let newButtons = new Array<Button>().concat(buttons)
-        newButtons.push({label: label, counter: {value: 0, plus: incrementUp, minus: incrementDown}})
+        newButtons.push({label: label, counter: {value: 0, plus: () => {increment(newButtons.length,1)}, minus: () incrementDown}})
         setButtons(newButtons)
     }
 
